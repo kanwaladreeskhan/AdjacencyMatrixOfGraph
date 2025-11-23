8f2da5e106bfc7ef895b8abb842cc3b76547cc77
kanwaladreeskhan <khanadreiskhan@gmail.com>
2025-11-23 09:51:55 +0500

    Commit to main

---
+++ AdjacencyMatrix/Graph.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdjacencyMatrix
+{
+    internal class Graph
+    {
+        private int[,] matrix;
+        private int vertices;
+        public Graph(int vertices)
+        {
+            this.vertices = vertices;
+            matrix=new int[vertices,vertices];
+        }
+        public void Add(int vertex1, int vertex2)
+        {
+            matrix[vertex1,vertex2] = 1;
+            matrix[vertex2,vertex1] = 1;
+        }
+        public void PrintGraph()
+        {
+            Console.WriteLine("Adjacency Matrix:");
+            for(int i = 0; i < vertices; i++)
+            {
+                for(int j = 0; j < vertices; j++)
+                {
+                    Console.Write(matrix[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ AdjacencyMatrix/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdjacencyMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph g = new Graph(5);
+            g.Add(0, 1);
+            g.Add(0, 4);
+            g.Add(1, 2);
+            g.Add(1, 4);
+            g.Add(2, 3);
+            g.Add(2, 4);
+            g.Add(3, 4);
+            g.PrintGraph();
+          }
+    }
+}
